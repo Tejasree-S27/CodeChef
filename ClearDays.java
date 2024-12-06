9ffa65827ea22d3e492e793f18efa21c1108ae7b
Tejasree-S27 <stejasree348@gmail.com>
2024-12-06 10:56:48 +0530

    Add files via upload

---
+++ ClearDays.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ClearDays
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+	    int cleardays=7-X-Y;
+	    System.out.println(cleardays);
+
+	}
+}
