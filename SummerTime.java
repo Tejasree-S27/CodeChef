18144526b401e4aca50a3cb66c55eeb11782d77a
Tejasree-S27 <stejasree348@gmail.com>
2024-12-06 10:57:53 +0530

    Add files via upload

---
+++ SummerTime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class SummerTime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>35)
+		{
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	}
+}
