17fbcc57ac2d2fcee09df2d3d7ae52caa093c1ad
Tejasree-S27 <stejasree348@gmail.com>
2024-12-08 21:03:30 +0530

    Add files via upload

---
+++ Acplz.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Acplz
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	int T=n.nextInt();
+	if(T>30)
+	System.out.println("YES");
+	else
+	System.out.println("NO");
+	}
+}
