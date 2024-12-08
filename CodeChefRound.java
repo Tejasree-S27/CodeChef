6cfdeb8b7dde549d2f3f874f7b73ecde168e490d
Tejasree-S27 <stejasree348@gmail.com>
2024-12-08 21:19:52 +0530

    Add files via upload

---
+++ CodeChefRound.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class CodeChefRound
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        if (N == 4) 
+        {
+            System.out.println("YES");
+        }
+        else 
+        {
+            System.out.println("NO");
+        }
+
+	}
+}
