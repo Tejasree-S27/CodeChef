de5f4ab7d2ec6d799b27f0538b2eb6cc5ffc0be5
Tejasree-S27 <stejasree348@gmail.com>
2024-12-08 21:24:36 +0530

    Add files via upload

---
+++ DoubleRent.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class DoubleRent
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        int finalrent = 2 * X;
+        System.out.println(finalrent);
+        
+	}
+}
