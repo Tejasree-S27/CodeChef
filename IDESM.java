93c1618b2a0f0ec453a3748cce8ef975d510efe4
Tejasree-S27 <stejasree348@gmail.com>
2024-12-08 21:16:01 +0530

    Add files via upload

---
+++ IDESM.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class IDESM
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        if (N == 15)
+        {
+            System.out.println("Yes");
+        } 
+        else 
+        {
+            System.out.println("No");
+        }
+        scanner.close();
+	}
+}
