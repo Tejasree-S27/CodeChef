d5418cb66f594f1d06e75dc2153ec9c549dd1729
Tejasree-S27 <stejasree348@gmail.com>
2024-12-09 22:17:18 +0530

    Add files via upload

---
+++ ChefAndSocks.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChefAndSocks
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        // Check if the points are greater than or equal to 12
+        if (X >= 12) 
+        {
+            System.out.println("Yes");
+        }
+        else 
+        {
+            System.out.println("No");
+        }
+        sc.close();
+	}
+}
