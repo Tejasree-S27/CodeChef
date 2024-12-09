64aa7a52c74bee5c43fc82c8f5249d486f578934
Tejasree-S27 <stejasree348@gmail.com>
2024-12-09 22:56:50 +0530

    Add files via upload

---
+++ MinimumWage.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class MinimumWage
+{
+	public static void main (String[] args) 
+    {
+		 Scanner sc= new Scanner(System.in);
+        int X=sc.nextInt();
+        // Minimum wage is 11 dollars per hour
+        if (X>11)
+        {
+            System.out.println("YES");
+        } 
+        else
+        {
+            System.out.println("NO");
+        }
+        sc.close();
+	}
+}
