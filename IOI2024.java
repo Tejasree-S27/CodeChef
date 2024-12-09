0b6d6cff508dde1538ad86b08d70a72f38f2028f
Tejasree-S27 <stejasree348@gmail.com>
2024-12-09 22:31:23 +0530

    Add files via upload

---
+++ IOI2024.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class IOI2024
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+        int X = sc.nextInt();
+        // IOI is ongoing from 1st to 8th of September
+        if (X >= 1 && X <= 8) 
+        {
+            System.out.println("YES");
+        }
+        else 
+        {
+            System.out.println("NO");
+        }
+        
+
+	}
+}
