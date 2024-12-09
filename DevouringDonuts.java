5fbba5c4e1fb32589504bb153938a5f3e20eb2e9
Tejasree-S27 <stejasree348@gmail.com>
2024-12-09 22:36:53 +0530

    Add files via upload

---
+++ DevouringDonuts.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class DevouringDonuts
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        // Calculate the total calories consumed by Chef
+        int totalCalories=X*Y;
+        // print the total calories
+        System.out.println(totalCalories);
+	}
+}
