8fcc7f2ad16167e172a94c57b40c3ff4868bbeb0
Tejasree-S27 <stejasree348@gmail.com>
2024-12-11 19:46:06 +0530

    Add files via upload

---
+++ IceCreamAndCone.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class IceCreamAndCone
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt(); 
+        int Y=sc.nextInt(); 
+        int max=Math.min(X, Y);
+        System.out.println(max);
+	}
+}
